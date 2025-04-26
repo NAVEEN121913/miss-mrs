ee3713761a2fae090a85dac08821432d30d1e0be
NAVEEN121913 <naveenkumarmsq@gmail.com>
2025-04-26 11:17:44 +0530

    text change

--- src/Component/Banner.jsx
+++ src/Component/Banner.jsx
@@ -19,7 +19,7 @@ const BannerWithOverlay = () => {
       {/* Overlay Text + Button */}
       <div className='absolute inset-0 flex flex-col items-center justify-center text-white bg-black/40'>
         <h1 className='text-3xl md:text-5xl font-bold mb-4'>
-          Welcome to Our Site
+          Grace in Every Thread
         </h1>
         <button
           className='bg-white text-black px-6 py-2 rounded hover:bg-gray-200 transition'
